9aa459fa54db07d892f0627c41d0160e763bcee3
kaesynock <kaesynock@gmail.com>
2024-04-19 09:46:46 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import CompanyList from "./pages/company/list";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
 
 
 
@@ -59,6 +62,11 @@ function App() {
                     </Authenticated>
                   }>
                     <Route index element={<Home />}/>
+                    <Route path="/companies">
+                      <Route index element={<CompanyList />} />
+                      <Route path="new" element={<Create />} />
+                      <Route path="edit/:id" element={<Edit />} />
+                    </Route>
                   </Route>
                 </Routes>
                 <RefineKbar />
